c36d6e4e88ebb84de9d10ed6312992ec0c096330
zellski1234 <zellski1234@gmail.com>
2022-08-08 15:17:57 +0100

    updated test

--- Day17/testmaxnum.js
+++ Day17/testmaxnum.js
@@ -1,7 +1,9 @@
-
-
 let Number1= Math.round(Math.random()*200);
+let Number2= Math.round(Math.random()*200);
+let Number3= Math.round(Math.random()*200);
 console.log(`Number1: ${Number1}`);
+console.log(`Number2: ${Number2}`);
+console.log(`Number3: ${Number3}`);
 
 
 //makes number1 back to 100 if it is above 100
@@ -16,7 +18,12 @@ function checkMaxNum(check){
         console.log("condition 0")
         return check;
     }
+    return check;
 }
 
-checkMaxNum(Number1);
+Number1 = checkMaxNum(Number1);
+Number2 = checkMaxNum(Number2);
+Number3 = checkMaxNum(Number3);
 console.log(`Number1: ${Number1}`);
+console.log(`Number2: ${Number2}`);
+console.log(`Number3: ${Number3}`);
